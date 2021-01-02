c47dca98afcddf2cdf267cc74be530eb0adce964
JOD <tj1998g@gmail.com>
2021-01-03 03:03:01 +0500

    fix
    fix

---
+++ script.js
@@ -0,0 +1,23 @@
+'use sctrict';
+
+const headerElem = document.querySelector('.header'),
+	addCarElem = document.querySelector('.add__car'),
+
+	addCarBrandElem = document.querySelector('.add__car-brand'),
+	addCarModelElem = document.querySelector('.add__car-model'),
+	addCarReleaseElem = document.querySelector('.add__car-release'),
+	addCarMileageElem = document.querySelector('.add__car-mileage'),
+	addCarBtnElem = document.querySelector('.add__car-btn'),
+
+	filter__car = document.querySelector('.filter__car'),
+
+	filterCarBrandElem = document.querySelector('.filter__car-brand'),
+	filterCarModelElem = document.querySelector('.filter__car-model'),
+	filterCarReleaseElem = document.querySelector('.filter__car-release'),
+	filterCarMileageElem = document.querySelector('.filter__car-mileage'),
+
+	tbodyNumElem = document.querySelector('.tbody__num'),
+	tbodyBrandElem = document.querySelector('.tbody__brand'),
+	tbodyModelElem = document.querySelector('.tbody__model'),
+	tbodyReleaseElem = document.querySelector('.tbody__release'),
+	tbodyMileageElem= document.querySelector('.tbody__mileage');
